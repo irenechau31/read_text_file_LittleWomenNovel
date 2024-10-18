8ec506f4eced341cce46af57d3d24d6195208a2a
irenechau31 <135424780+irenechau31@users.noreply.github.com>
2024-10-18 18:14:01 -0400

    Add files via upload
    Added code comments to explain the codes

--- Read text fille_Little women novel.py
+++ Read text fille_Little women novel.py
@@ -8,21 +8,28 @@ Created on Fri Oct 18 12:02:07 2024
 from collections import Counter as Counter
 import pandas as pd
 
-# Open the file with utf-8-sig to handle BOM
+# Open the file with utf-8-sig to handle BOM '\theff'
 file = open(r'C:\Users\User\OneDrive\Desktop\Fordham\Programming with Python (CISC-5380-L01)\Little women novel.txt', 'r', encoding='utf-8-sig')
-lines = file.readlines()
+lines = file.readlines() #using readlines() in stead of readline() to read multiple lines
 punctuation = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
+
+#create an empty list of cleaned_words to store cleaned words without punctuations
 cleaned_words=[]
-for line in lines:
-    words=line.split()
-    for word in words:
-        cleaned_word=''
-        for char in word:
+for line in lines: #read each line in lines 
+    words=line.split() #splitting words in that line, output a list of words
+    for word in words: #for each word in that list of words
+    
+        #create an empty str to store the cleaned word after checking each of characters in the word
+        cleaned_word='' 
+        for char in word: #for each char in that word
+        #if the character is not punctuation -> add the char to empty str cleaned_word
             if char not in punctuation:
                 cleaned_word+=char
+                
 #code may add empty strings to cleaned_words, especially if any words consist only of punctuation.
         if cleaned_word: # Only add non-empty cleaned words
             cleaned_words.append(cleaned_word.lower())
+            
 file.close()
 print(cleaned_words)
 # Count the total number of the words
